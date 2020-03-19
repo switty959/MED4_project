f595572f8c1348e7a22f12f10ba63db15a6675cf
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2020-03-19 19:30:23 +0100

    test maze
    first test of movement in maze without pid input

---
+++ Unity/New Unity Project/Assets/Scripts/followPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public GameObject player;       //Public variable to store a reference to the player game object
+    private string playerTag = "Player";
+
+    Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag(playerTag);
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void Update()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Unity/New Unity Project/Assets/Scripts/movementScript.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public Vector3 movement;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector3 direction)
+    {
+        rb.MovePosition((Vector3)transform.position + (direction * movementSpeed * Time.deltaTime));
+    }
+}
