f1f2af7054b26c821adf199d8750aa7b71ba38d3
Scott <42110788+ScottJamesNaylor@users.noreply.github.com>
2020-04-14 19:53:04 +0200

    Time and Distance
    Time and distance counters have been implemented on the player movement script :)

--- Unity/Med4Project/Assets/Scripts/movementScript.cs
+++ Unity/Med4Project/Assets/Scripts/movementScript.cs
@@ -10,16 +10,23 @@ public class movementScript : MonoBehaviour
     public float movementSpeed;
     public Vector3 movement;
     Rigidbody rb;
+    public float distanceCounter;
+    public float timeCounter;
+    public bool ended;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
+        
     }
 
     private void Update()
     {
         movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (distanceCounter > 0 && ended != true) timeCounter += Time.deltaTime;
 
     }
 
@@ -31,5 +38,17 @@ public class movementScript : MonoBehaviour
     void moveCharacter(Vector3 direction)
     {
         rb.MovePosition((Vector3)transform.position + (direction * movementSpeed * Time.deltaTime));
+        distanceCounter += Mathf.Abs(direction.x + direction.z);
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "End")
+        {
+            ended = true;
+        }
     }
 }
