3bd95602d813ec6b7f792bb860292e5959759980
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2020-04-16 13:35:20 +0200

    menu made

---
+++ Unity/Med4Project/Assets/Scripts/buttonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonController : MonoBehaviour
+{
+    public GameObject loadingImage;
+    public GameObject[] buttonPanels;
+   public void startGame(int level)
+    {
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(level);
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void instructions()
+    {
+        buttonPanels[0].SetActive(false); //turning the main menu  off
+        buttonPanels[1].SetActive(true); // turning the instruction menu on
+    }
+
+    public void mainMenu()
+    {
+        buttonPanels[1].SetActive(false); //turning the main menu on
+        buttonPanels[0].SetActive(true); // turning the instruction menu off
+    }
+}
